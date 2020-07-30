e82489aef113b7c6abc9e7847a49ef9ef290e218
peterquintas <peter@quintas.org>
2020-07-30 13:46:18 -0400

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,6 +4,10 @@ set -e
 
 echo "$(date) - Start"
 
-aws s3 sync /data s3://$BUCKET$BUCKET_PATH $PARAMS
+if [[ "$1" == 'down' ]]; then
+  aws s3 sync s3://$BUCKET$BUCKET_PATH /data $PARAMS
+else
+  aws s3 sync /data s3://$BUCKET$BUCKET_PATH $PARAMS
+fi
 
 echo "$(date) End"
